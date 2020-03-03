077724304410b40a55c80bb8751e2a1102827811
Piotr Bielenia <piotrbielenia0@gmail.com>
2020-03-03 18:22:07 +0100

    Refactoring step

--- src/include/Soundex.hpp
+++ src/include/Soundex.hpp
@@ -9,6 +9,9 @@ public:
     std::string encode(const std::string& word) const;
 
 private:
+    std::string head(const std::string& word) const;
+    std::string encodedDigits(const std::string& word) const;
+    std::string encodedDigit() const;
     std::string zeroPad(const std::string& word) const;
 
 };
--- src/src/Soundex.cpp
+++ src/src/Soundex.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "Soundex.hpp"
 
+static const size_t max_code_length{4};
+
 std::string Soundex::encode(const std::string& word) const
 {
-    return zeroPad(word);
+    return zeroPad(head(word) + encodedDigits(word));
+}
+
+std::string Soundex::head(const std::string& word) const
+{
+    return word.substr(0, 1);
+}
+
+std::string Soundex::encodedDigits(const std::string& word) const
+{
+    if (word.length() > 1) return encodedDigit();
+    return "";
+}
+
+std::string Soundex::encodedDigit() const
+{
+    return "1";
 }
 
 std::string Soundex::zeroPad(const std::string& word) const
 {
-    return word + "000";
+    auto zeros_needed = max_code_length - word.length();
+    return word + std::string(zeros_needed, '0');
 }
